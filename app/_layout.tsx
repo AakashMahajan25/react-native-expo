f0312c37591f4f4ac48252da6006fe2a05cf2abf
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-03-12 18:01:34 +0530

    feat: GlueStack UI Added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { StyleSheet, Text, View } from 'react-native';
+// import { Stack } from "expo-router";
 
 export default function RootLayout() {
-    return <Stack />;
+    return <GluestackUIProvider mode="light"><View style={styles.container}>
+        <Text>Hello World!</Text>;
+        </View></GluestackUIProvider>
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
